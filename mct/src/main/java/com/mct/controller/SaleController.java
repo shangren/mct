b5ae09d8e672d5c705e32f090c1632f3d20dafe0
wangyangchao <827741251@qq.com>
2016-06-16 10:37:08 +0800

    description:商人

---
+++ mct/src/main/java/com/mct/controller/SaleController.java
@@ -0,0 +1,14 @@
+package com.mct.controller;
+
+/**
+ * 
+ * @author yangchao.wang
+ *
+ * @email  yangchao.wang@flaginfo.com.cn
+ * @Desc  
+ * 
+ * @time 2016年6月16日上午10:35:20
+ */
+public class SaleController {
+
+}
