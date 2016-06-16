4bac094c32277b8b5ce63edc8accf9fccc4f7cfb
wangyangchao <827741251@qq.com>
2016-06-16 10:41:39 +0800

    描述

--- mct/src/main/java/com/mct/controller/UserController.java
+++ mct/src/main/java/com/mct/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.mct.controller;
-
+/**
+ * 
+ * @author yangchao.wang
+ *
+ * @email  yangchao.wang@flaginfo.com.cn
+ * @Desc  
+ * 用户
+ * @time 2016年6月16日上午10:41:13
+ */
 public class UserController {
 
 }
